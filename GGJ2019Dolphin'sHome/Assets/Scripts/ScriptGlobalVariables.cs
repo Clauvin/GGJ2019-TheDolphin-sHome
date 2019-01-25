e18fb87a6e257f813cbfcea764b656cd11cdf307
Clauvin <clauvin_almeida@hotmail.com>
2019-01-25 21:02:35 -0200

    Created ScriptGlobalVariables

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptGlobalVariables.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptGlobalVariables : MonoBehaviour {
+
+    public float game_speed = 1.0f;
+    public int which_game_stage = 1;
+
+}
