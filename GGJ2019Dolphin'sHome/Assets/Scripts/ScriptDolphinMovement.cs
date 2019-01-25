78e1f3179e480596e6546f4e033ecf8e97be6fc4
Clauvin <clauvin_almeida@hotmail.com>
2019-01-25 21:16:34 -0200

    Starting Dolphin Rotation

--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptDolphinMovement.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptDolphinMovement.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class ScriptDolphinMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private GameObject root_player;
+    public Vector3 scalar_velocity;
+    public float z_angle;
+
+    private void Awake()
+    {
+        root_player = GameObject.FindGameObjectWithTag("Player");
+        z_angle = root_player.transform.rotation.z;
+    }
+
+    // Use this for initialization
+    void Start () {
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	
 	}
+
+    private void ControlDolphin()
+    {
+
+    }
+
+    private void RotateDolphin(float rotation_in_degrees)
+    {
+
+    }
 }
