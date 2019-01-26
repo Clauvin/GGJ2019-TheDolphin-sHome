e0da0b602508499fe0cbc7a5c1e3e27994308496
Clauvin <clauvin_almeida@hotmail.com>
2019-01-26 11:42:16 -0200

    Update ScriptDolphinStamina.cs

--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptDolphinStamina.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptDolphinStamina.cs
@@ -8,6 +8,9 @@ public class ScriptDolphinStamina : MonoBehaviour {
     public float actual_stamina_value;
     public float max_stamina_value;
 
+    public float stamina_decay;
+    public float partial_stamina_recovery;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,6 +18,31 @@ public class ScriptDolphinStamina : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        StaminaFall();
+        InCaseOfNoStamina();
+    }
+
+    public void StaminaFall()
+    {
+        actual_stamina_value -= stamina_decay * ScriptGlobalVariables.game_speed;
+        if (actual_stamina_value <= min_stamina_value) actual_stamina_value = min_stamina_value;
+    }
+
+    public void PartialStaminaRecover()
+    {
+        actual_stamina_value += partial_stamina_recovery;
+        if (actual_stamina_value >= max_stamina_value) actual_stamina_value = max_stamina_value;
+    }
+
+    public void FullStaminaRecover()
+    {
+        actual_stamina_value = max_stamina_value;
+    }
+
+    public void InCaseOfNoStamina()
+    {
+
+    }
+
+
 }
