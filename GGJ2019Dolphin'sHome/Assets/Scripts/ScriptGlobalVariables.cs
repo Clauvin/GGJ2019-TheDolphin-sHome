3980af2e513924cd97e628539b127e00ac493551
Clauvin <clauvin_almeida@hotmail.com>
2019-01-26 03:28:35 -0200

    Added player and water to GlobalVariables

--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptGlobalVariables.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptGlobalVariables.cs
@@ -6,5 +6,6 @@ public static class ScriptGlobalVariables{
 
     public static float game_speed = 1.0f;
     public static int which_game_stage = 1;
-
+    public static GameObject player;
+    public static GameObject water;
 }
