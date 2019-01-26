979f8cc9fa4f3c57b1aec2df5575b62540d7a2cd
Clauvin <clauvin_almeida@hotmail.com>
2019-01-26 03:27:17 -0200

    Added ScriptGlobalStarter

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptGlobalStarter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptGlobalStarter : MonoBehaviour {
+
+    void Awake()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        ScriptGlobalVariables.player = GameObject.FindGameObjectWithTag("Player");
+        ScriptGlobalVariables.water = GameObject.FindGameObjectWithTag("Water");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
