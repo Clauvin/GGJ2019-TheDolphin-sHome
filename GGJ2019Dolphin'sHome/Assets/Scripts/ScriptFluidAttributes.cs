199843f0d1b96645c5a1102dade1b211da261ea3
Clauvin <clauvin_almeida@hotmail.com>
2019-01-26 13:50:03 -0200

    Sistema de alteração de cor funcionando

--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptFluidAttributes.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptFluidAttributes.cs
@@ -6,13 +6,55 @@ public class ScriptFluidAttributes : MonoBehaviour {
 
     public float friction;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private Color initial_color;
+    public Color final_color;
+    public bool there_will_be_a_color_change;
+    public float change_counter;
+    public float time_for_change;
+
+    void Awake()
+    {
+        initial_color = GetComponent<MeshRenderer>().material.GetColor("_Color");
+    }
+
+    // Use this for initialization
+    void Start () {
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (there_will_be_a_color_change)
+        {
+            ChangeColorGradually(final_color);
+            change_counter += Time.deltaTime;
+            if (change_counter >= time_for_change) there_will_be_a_color_change = false;
+        }
 	}
+
+    protected void ChangeColor(Color color)
+    {
+        Material material = GetComponent<MeshRenderer>().material;
+        material.SetColor("_Color", color);
+    }
+
+    protected void ChangeColorGradually(Color final_color)
+    {
+        Color cor_original = initial_color;
+
+        Color cor_final = final_color;
+
+        float coeficient_original = (time_for_change - change_counter) / time_for_change;
+        if (coeficient_original < 0) coeficient_original = 0;
+
+        float coeficient_final = change_counter / time_for_change;
+        if (coeficient_final > 1) coeficient_original = 1;
+
+        Color cor_de_transicao = coeficient_original * cor_original + coeficient_final * cor_final;
+        Debug.Log("Cor_original " + cor_original);
+        Debug.Log("Cor final " + cor_final);
+        Debug.Log("Cor de transição " + cor_de_transicao);
+
+        GetComponent<MeshRenderer>().material.SetColor("_Color", cor_de_transicao);
+    }
 }
--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptWaterAttributes.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptWaterAttributes.cs
@@ -6,11 +6,16 @@ public class ScriptWaterAttributes : ScriptFluidAttributes {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        if (there_will_be_a_color_change)
+        {
+            ChangeColorGradually(final_color);
+            change_counter += Time.deltaTime;
+            if (change_counter >= time_for_change) there_will_be_a_color_change = false;
+        }
+    }
 }
