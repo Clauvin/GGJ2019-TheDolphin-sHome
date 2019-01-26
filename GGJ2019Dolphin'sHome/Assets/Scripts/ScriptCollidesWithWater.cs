7e89577d40d77421062fea39e48b717f3b5ceef8
Clauvin <clauvin_almeida@hotmail.com>
2019-01-26 03:26:55 -0200

    Added ScriptCollidesWithWater

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptCollidesWithWater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCollidesWithWater : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<TagWater>())
+        {
+            ScriptGlobalVariables.player.GetComponentInChildren<ScriptDolphinMovement>().is_in_water = true;
+        }
+        else ScriptGlobalVariables.player.GetComponentInChildren<ScriptDolphinMovement>().is_in_water = false;
+    }
+}
