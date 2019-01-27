59e040f70388dc31dae504affe102a70395cf980
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 15:52:10 -0200

    8m, but we have some extra time

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Dolphin/ScriptIfFullJumpToConversa1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIfFullJumpToConversa1 : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (GetComponent<ScriptDolphinStamina>().actual_stamina_value ==
+                GetComponent<ScriptDolphinStamina>().max_stamina_value)
+        {
+            SceneManager.LoadScene.LoadConversaSobreLar1();
+        }
+    }
+}
