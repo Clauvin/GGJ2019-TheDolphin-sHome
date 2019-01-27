90daebc7e8bdfde4cab5a72a719d5995b05580f0
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 12:25:35 -0200

    3 hours and 35 minutes

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Dolphin/ScriptCollidesWithBarrier.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCollidesWithBarrier : MonoBehaviour {
+
+    public GameObject[] sons_do_boto;
+    public GameObject communication_image;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<TagBarrier>())
+        {
+            int qual_som = (int)Random.Range(0, 3);
+
+            sons_do_boto[qual_som].GetComponent<AudioSource>().Play();
+
+            Instantiate(communication_image, transform.parent);
+
+
+            //Spawnar imagem que dura 8 segundos.
+        }
+    }
+
+}
---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptVisualEffect.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptVisualEffect : MonoBehaviour {
+
+    public float minimum_effect;
+    public float maximum_effect;
+
+    public float counter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        TimeUpdate();
+        SphereUpdate();
+    }
+
+    void TimeUpdate()
+    {
+        counter += Time.deltaTime;
+    }
+
+    void SphereUpdate()
+    {
+        float radius = minimum_effect + counter * 6 % (maximum_effect - minimum_effect);
+        Vector3 new_size = new Vector3(radius, radius, 10);
+
+        transform.localScale = new_size;
+    }
+}
