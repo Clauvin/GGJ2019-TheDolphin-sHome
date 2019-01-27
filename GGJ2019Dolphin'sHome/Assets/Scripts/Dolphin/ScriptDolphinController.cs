f4304055837f2da7a4602c494ba2819485032d7c
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 01:33:15 -0200

    Animação Normal e Travada funcionando

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Dolphin/ScriptDolphinController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptDolphinController : MonoBehaviour {
+
+    ScriptDolphinMovement dolphin_movement;
+
+    // Use this for initialization
+	void Start () {
+
+        dolphin_movement = transform.parent.GetComponentInChildren<ScriptDolphinMovement>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ChooseAnimation();
+    }
+
+    void ChooseAnimation()
+    {
+        if (dolphin_movement.stun_time > 0)
+        {
+            GetComponent<Animator>().ResetTrigger("MudarParaSituaçãoNormal");
+            GetComponent<Animator>().SetTrigger("MudarParaTravado");
+        }
+        else
+        {
+            GetComponent<Animator>().ResetTrigger("MudarParaTravado");
+            GetComponent<Animator>().SetTrigger("MudarParaSituaçãoNormal");
+        }
+    }
+}
