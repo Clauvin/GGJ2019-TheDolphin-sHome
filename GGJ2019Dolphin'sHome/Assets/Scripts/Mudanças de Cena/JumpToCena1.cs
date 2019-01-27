95eee0ff3d249f9908ed2c70e0b3bbb1a8ea474b
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 15:02:20 -0200

    1h

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Mudanças de Cena/JumpToCena1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpToCena1 : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider other)
+    {
+        SceneManager.LoadScene.LoadFase1();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
