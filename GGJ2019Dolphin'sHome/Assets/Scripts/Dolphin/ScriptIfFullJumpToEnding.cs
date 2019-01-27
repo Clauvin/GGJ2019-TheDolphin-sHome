3e64044e0aed13d8fa0ca415bcb1112904bf5d82
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 17:19:55 -0200

    TIME TO SEND AND PRAY THAT WE HAVE ENOUGH EXTRA TIME

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Dolphin/ScriptIfFullJumpToEnding.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIfFullJumpToEnding : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (GetComponent<ScriptDolphinStomach>().max_stomach_limit ==
+                GetComponent<ScriptDolphinStomach>().how_much_filled)
+        {
+            SceneManager.LoadScene.LoadConclusão();
+        }
+    }
+}
---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptWhenVideoEndsGoToTutorial.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental;
+using UnityEngine.Video;
+
+public class ScriptWhenVideoEndsGoToTutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!GetComponent<VideoPlayer>().isPlaying)
+        {
+            SceneManager.LoadScene.LoadTutorial();
+        }
+	}
+}
