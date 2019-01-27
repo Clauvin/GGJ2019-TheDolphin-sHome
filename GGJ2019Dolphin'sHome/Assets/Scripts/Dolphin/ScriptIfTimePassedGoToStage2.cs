8c1d29f5a8891c78fcbd814672353b7733a94649
Clauvin <clauvin_almeida@hotmail.com>
2019-01-27 16:25:21 -0200

    35m

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Dolphin/ScriptIfTimePassedGoToStage2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIfTimePassedGoToStage2 : MonoBehaviour {
+
+    public float counter;
+    public float time_to_jump;
+	
+	// Update is called once per frame
+	void Update () {
+        CounterUpdate();
+        TimeToJump();
+    }
+
+    void CounterUpdate()
+    {
+        counter += Time.deltaTime;
+    }
+
+    void TimeToJump()
+    {
+        if (counter >= time_to_jump)
+        {
+            SceneManager.LoadScene.LoadFase2();
+        }
+    }
+}
