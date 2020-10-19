7d2f9dc8d8ae52e70454538304e9caf98709b53b
Cláuvin <Clauvin@users.noreply.github.com>
2020-10-18 22:51:26 -0300

    (#2) SceneManager script creation in progress

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/SceneManager/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public static void LoadConversaSobreLar1()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetSceneByName("ConversaSobreLar1").name);
+    }
+
+    public static void LoadCréditos()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetSceneByName("Créditos").name);
+    }
+
+    public static void ResetScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(
+            UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
