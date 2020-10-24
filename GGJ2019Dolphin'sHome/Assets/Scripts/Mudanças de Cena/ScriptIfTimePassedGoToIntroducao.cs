67f415821cc0db46454dfa13591e75c0646054b2
Cláuvin <Clauvin@users.noreply.github.com>
2020-10-24 19:50:55 -0300

    (#9) Created ScriptIfTimePassedGoToIntroducao

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Mudanças de Cena/ScriptIfTimePassedGoToIntroducao.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIfTimePassedGoToIntroducao : MonoBehaviour {
+
+    public float counter;
+    public float time_to_jump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CounterUpdate();
+        TimeToJump();
+    }
+
+    void CounterUpdate()
+    {
+        counter += Time.deltaTime;
+    }
+
+    void TimeToJump()
+    {
+        if (counter >= time_to_jump)
+        {
+            SceneManager.LoadIntroducao();
+        }
+    }
+}
