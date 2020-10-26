0af986b95b62bdc0dac65b317f1ae0b0d82a9455
Cláuvin <Clauvin@users.noreply.github.com>
2020-10-26 00:17:28 -0300

    (#7) Created ScriptIfTimePassedCloseGame

---
+++ GGJ2019Dolphin'sHome/Assets/Scripts/Mudanças de Cena/ScriptIfTimePassedCloseGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptIfTimePassedCloseGame : MonoBehaviour {
+
+    public float counter;
+    public float time_to_jump;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CounterUpdate();
+        TimeToJump();
+    }
+
+    void CounterUpdate()
+    {
+        counter += Time.deltaTime;
+    }
+
+    void TimeToJump()
+    {
+        if (counter >= time_to_jump)
+        {
+            Application.Quit();
+        }
+    }
+
+}
