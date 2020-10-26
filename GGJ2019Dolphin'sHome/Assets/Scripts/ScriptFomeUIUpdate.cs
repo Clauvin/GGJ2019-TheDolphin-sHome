1dbe554c0b1a3e748bceb7471498a799a1276db5
Cláuvin <Clauvin@users.noreply.github.com>
2020-10-25 21:19:52 -0300

    (#6) Added code to set the UI properly

--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptFomeUIUpdate.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptFomeUIUpdate.cs
@@ -6,6 +6,8 @@ public class ScriptFomeUIUpdate : MonoBehaviour {
 
     ScriptDolphinStomach dolphin_stomach;
 
+    public bool gambiarra = true;
+
     // Use this for initialization
     void Start()
     {
@@ -23,5 +25,27 @@ public class ScriptFomeUIUpdate : MonoBehaviour {
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, amount_of_hunger);
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, amount_of_hunger);
 
+        if (gambiarra)
+        {
+            ChangeDistanciaPosition();
+            gambiarra = false;
+        }
+
+    }
+
+    private void ChangeDistanciaPosition()
+    {
+        Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        ScriptFixedRatio fixed_ratio = camera.GetComponent<ScriptFixedRatio>();
+        float x = Screen.width; float y = Screen.height;
+        float percentage_of_x = camera.rect.x; float percentage_of_y = camera.rect.y;
+
+        float extra_position_x = x * percentage_of_x; float extra_position_y = y * percentage_of_y;
+        extra_position_y *= -1;
+
+        Vector3 new_position = GetComponent<RectTransform>().position;
+        new_position.x += extra_position_x;
+        new_position.y += extra_position_y;
+        GetComponent<RectTransform>().position = new_position;
     }
 }
--- GGJ2019Dolphin'sHome/Assets/Scripts/ScriptStaminaUIUpdate.cs
+++ GGJ2019Dolphin'sHome/Assets/Scripts/ScriptStaminaUIUpdate.cs
@@ -6,6 +6,8 @@ public class ScriptStaminaUIUpdate : MonoBehaviour {
 
     ScriptDolphinStamina dolphin_stamina;
 
+    public bool gambiarra = true;
+
     void Awake()
     {
         
@@ -26,5 +28,27 @@ public class ScriptStaminaUIUpdate : MonoBehaviour {
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, amount_of_stamina);
         GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, amount_of_stamina);
 
+        if (gambiarra)
+        {
+            ChangeDistanciaPosition();
+            gambiarra = false;
+        }
+
+    }
+
+    private void ChangeDistanciaPosition()
+    {
+        Camera camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        ScriptFixedRatio fixed_ratio = camera.GetComponent<ScriptFixedRatio>();
+        float x = Screen.width; float y = Screen.height;
+        float percentage_of_x = camera.rect.x; float percentage_of_y = camera.rect.y;
+
+        float extra_position_x = x * percentage_of_x; float extra_position_y = y * percentage_of_y;
+        extra_position_y *= -1;
+
+        Vector3 new_position = GetComponent<RectTransform>().position;
+        new_position.x += extra_position_x;
+        new_position.y += extra_position_y;
+        GetComponent<RectTransform>().position = new_position;
     }
 }
